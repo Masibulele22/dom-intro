dc6555d1367557c4805f1b32ba4b1334e071317f
Masibulele22 <41905558+Masibulele22@users.noreply.github.com>
2019-06-03 15:25:03 +0200

    Update calculate-bill.js

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -11,3 +11,33 @@
 //  * once done looping over all the entries - display the total onto the screen in the billTotal element
 
 //link the function to a click event on the calculate button
+
+var billStringElement = document.querySelector(".billStrng");
+
+function calculateBtnClicked(){
+}
+
+calculateBtn.addEventListener('click', calculateBtnClicked);
+
+function totalPhoneBill(str) {
+    var totalSms = 0;
+  var totalCalls = 0;
+    var str2 = str.split(', ');
+    console.log(str2);
+    for (var i=0; i<str2.length; i++) {
+      var str3 = str2[i];
+      if (str3 == 'sms') {
+        totalSms++
+      }
+      else if (str3 == 'call') {
+      totalCalls++
+      }
+  }
+    return ('R' + (totalSms*0.65 + totalCalls*2.75).toFixed(2));
+}
+
+function calculateBtnClicked(){
+    var billString = billStringField.value;   
+    var roundedBillTotal = totalPhoneBill(billString);
+    billTotalElement.innerHTML = roundedBillTotal;
+}
